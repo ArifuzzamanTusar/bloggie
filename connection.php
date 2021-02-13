1ea099e141bb0009b06f26ee7a636c7e32b2dc74
Arifuzzaman Tusar <arifuzzamantusar50@gmaill.com>
2021-02-13 16:20:10 +0600

    contact page + contact DB

--- connection.php
+++ connection.php
@@ -11,16 +11,3 @@ if (!$con) {
 }
 
 
-$query = "SELECT * FROM `categories`LIMIT 1";
-$result = mysqli_query($con, $query);
-
-
-
-if (mysqli_num_rows($result) > 0) {
-
-    while ($row = mysqli_fetch_assoc($result)) {
-        echo $row["name"]."<br>";
-       
-    }
-}
-
