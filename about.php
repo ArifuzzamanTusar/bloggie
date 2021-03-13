b52b7972be8d01268589ae12b0b042e31eab3a46
Arifuzzaman Tusar <arifuzzamantusar50@gmaill.com>
2021-03-13 21:26:33 +0600

    Update about.php

--- about.php
+++ about.php
@@ -7,12 +7,18 @@ $page_tittle = "About";
 
 <!-- Vendor CSS Files -->
 <!-- <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet"> -->
-<link href="assets/vendor/icofont/icofont.min.css" rel="stylesheet">
+<link href="assets/vendor/icofont/icofont.min.css" rel="stylesheet"> 
+<!-- icon library  -->
 <link href="assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+<!-- icon library  -->
 <link href="assets/vendor/remixicon/remixicon.css" rel="stylesheet">
-<link href="assets/vendor/venobox/venobox.css" rel="stylesheet">
+<!-- icon library  -->
+<!-- <link href="assets/vendor/venobox/venobox.css" rel="stylesheet"> -->
+<!-- dorkar nai  -->
 <link href="assets/vendor/owl.carousel/assets/owl.carousel.min.css" rel="stylesheet">
+<!-- carousl library  -->
 <link href="assets/vendor/aos/aos.css" rel="stylesheet">
+<!-- animation library  -->
 <!-- This page Main CSS File -->
 <link href="assets/css/style.css" rel="stylesheet">
 <div class="container-fluid">
@@ -605,10 +611,10 @@ $page_tittle = "About";
 <!-- including footers -->
 <?php include 'footer.php'; ?>
 <!-- Vendor JS Files -->
-<script src="assets/vendor/jquery/jquery.min.js"></script>
-<script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+<!-- <script src="assets/vendor/jquery/jquery.min.js"></script> -->
+<!-- <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script> -->
 <script src="assets/vendor/jquery.easing/jquery.easing.min.js"></script>
-<script src="assets/vendor/php-email-form/validate.js"></script>
+<!-- <script src="assets/vendor/php-email-form/validate.js"></script> -->
 <script src="assets/vendor/waypoints/jquery.waypoints.min.js"></script>
 <script src="assets/vendor/counterup/counterup.min.js"></script>
 <script src="assets/vendor/venobox/venobox.min.js"></script>
